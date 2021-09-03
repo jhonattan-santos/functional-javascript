d1b94b744c587ee455f059491a019373687623be
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2021-09-02 23:40:11 -0300

    Added the methods to count, split and group words

--- challenges/01/functions.js
+++ challenges/01/functions.js
@@ -1,10 +1,110 @@
 const fs = require("fs");
+const path = require("path");
 
 function lerDiretorio(caminho) {
-  let arquivos = fs.readdirSync(caminho);
-  console.log(arquivos);
+  return new Promise((resolve, reject) => {
+    try {
+      let arquivos = fs.readdirSync(caminho);
+      arquivos = arquivos.map((arquivo) => path.join(caminho, arquivo));
+      resolve(arquivos);
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+function elementosTerminadosCom(padraoTextual) {
+  return function (array) {
+    return array.filter((arquivo) => arquivo.endsWith(padraoTextual));
+  };
+}
+
+function lerArquivo(caminho) {
+  return new Promise((resolve, reject) => {
+    try {
+      const conteudo = fs.readFileSync(caminho, "utf-8");
+      resolve(conteudo.toString());
+    } catch (error) {
+      reject(e);
+    }
+  });
+}
+
+function lerArquivos(caminhos) {
+  return Promise.all(caminhos.map((caminho) => lerArquivo(caminho)));
+}
+
+function removerElementosSeVazio(array) {
+  return array.filter((el) => el.trim());
+}
+
+function removerElementosSeIncluir(padraoTextual) {
+  return function (array) {
+    return array.filter((el) => !el.includes(padraoTextual));
+  };
+}
+
+function removerElementosSeApenasNumeros(array) {
+  return array.filter((el) => {
+    const num = parseInt(el.trim());
+    return num !== num;
+  });
+}
+
+function removerSimbolos(simbolos) {
+  return function (array) {
+    return array.map((el) => {
+      let textoSemSimbolos = el;
+      simbolos.forEach((simbolo) => {
+        textoSemSimbolos = textoSemSimbolos.split(simbolo).join("");
+      });
+      return textoSemSimbolos;
+    });
+  };
+}
+
+const mesclarElementos = (conteudos) => conteudos.join(" ");
+
+const separarPor = (padraoTextual) => {
+  return function (todoConteudo) {
+    return todoConteudo.split(padraoTextual);
+  };
+};
+
+function agruparElementos(palavras) {
+  return Object.values(
+    palavras.reduce((acc, palavra) => {
+      const el = palavra.toLowerCase();
+      const qtde = acc[el] ? acc[el].qtde + 1 : 1;
+      acc[el] = {
+        elemento: el,
+        qtde,
+      };
+
+      return acc;
+    }, {})
+  );
+}
+
+function ordenarPorAtribNumerico(attr, ordem = "asc") {
+  return function (array) {
+    const asc = (o1, o2) => o1[attr] - o2[attr];
+    const desc = (o1, o2) => o2[attr] - o1[attr];
+    return array.sort(ordem === "asc" ? asc : desc);
+  };
 }
 
 module.exports = {
   lerDiretorio,
+  lerArquivo,
+  lerArquivos,
+  elementosTerminadosCom,
+  removerElementosSeVazio,
+  removerElementosSeIncluir,
+  removerElementosSeApenasNumeros,
+  removerSimbolos,
+  mesclarElementos,
+  separarPor,
+  agruparElementos,
+  ordenarPorAtribNumerico,
 };
--- challenges/01/index.js
+++ challenges/01/index.js
@@ -3,4 +3,37 @@ const fn = require("./functions");
 
 const caminho = path.join(__dirname, "legendas");
 
-fn.lerDiretorio(caminho);
+const simbolos = [
+  ".",
+  "?",
+  "-",
+  ",",
+  '"',
+  "_",
+  "<i>",
+  "</i>",
+  "♪",
+  "\r",
+  "[",
+  "]",
+  "(",
+  ")",
+];
+
+const arquivos = fn
+  .lerDiretorio(caminho)
+  .then(fn.elementosTerminadosCom(".srt"))
+  .then(fn.lerArquivos)
+  .then(fn.mesclarElementos)
+  .then(fn.separarPor("\n"))
+  .then(fn.removerElementosSeVazio)
+  .then(fn.removerElementosSeIncluir("-->"))
+  .then(fn.removerElementosSeApenasNumeros)
+  .then(fn.removerSimbolos(simbolos))
+  .then(fn.mesclarElementos)
+  .then(fn.separarPor(" "))
+  .then(fn.removerElementosSeVazio)
+  .then(fn.removerElementosSeApenasNumeros)
+  .then(fn.agruparElementos)
+  .then(fn.ordenarPorAtribNumerico("qtde", "desc"))
+  .then(console.log);
